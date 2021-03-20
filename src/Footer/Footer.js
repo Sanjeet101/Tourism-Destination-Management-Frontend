ee8e06d2defb0d228049254c37f02bc1eefdac62
Sanjeet <sanjeetpandey010@gmail.com>
2021-03-20 16:24:33 +0545

    minor changed on footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,8 +6,10 @@ class Footer extends Component{
         <footer>
           <div class="bottom">
               <center>
-                <span class="credit">Created By <a href="/">Sanjeet Pandey</a> | </span>
-                <span class="far fa-copyright"></span><span> 2020 All rights reserved.</span>
+              <span class="credit">Trip Planner <a href="/">Your Trip (c) 2021 </a> | </span>
+                <span class="credit">Created By <a href="">Sanjeet Pandey</a> | </span>
+                <span class="far fa-copyright"></span><span></span>
+                <span class="far fa-copyright"></span><span> 2021 All rights reserved.</span>
               </center>
             </div>
           </footer>
