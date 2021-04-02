9c270f890256bb7e02e628215c2db3c1f0461546
Sanjeet <sanjeetpandey010@gmail.com>
2021-04-02 22:06:53 +0545

    first blog added in blog page

--- src/Body/Blog.js
+++ src/Body/Blog.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react'
 class Blog extends Component{
     render(){
         return(
-            <div>
-            
-                </div>
+            <div class="blog-post">
+	<div class="blog-hero"></div>
+		<article>
+			
+			<div class="blog-content">
+				<summary>
+					<h3>My First Blog Post</h3>
+					<div class="blog-date">23 Jan 2021</div>
+				</summary>
+				<p></p>
+			</div>
+			<div class="blog-img">
+				<div class="blog-img-caption"><b>Pictured</b> Some Cool Skyline</div>
+			</div>
+			<div class="blog-content">
+				<p></p>
+
+				<p> </p>
+			</div>	
+	</article>
+</div>
         )
     }
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-class Header extends Component{
-    render(){
+class Header extends Component {
+    render() {
         // logout = ()=>{
         //     localStorage.removeItem('token')
         //     localStorage.removeItem('id')
@@ -15,34 +15,34 @@ class Header extends Component{
         //     <div className="collapse navbar-collapse" id="navbarResponsive">
         //     <ul className="navbar-nav ml-auto">
         //        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/destinations"><b>Destinations</b></a></li>
-              
+
         //         </ul>
         // </div>
         // }
-        return(
+        return (
             <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                 <img src="images/logo.png" className="logo" alt=""></img>
-            <div className="container">
-                <a className="navbar-brand js-scroll-trigger" href="/"><b>Trip Planner</b></a>
-                
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
+                <div className="container">
+                    <a className="navbar-brand js-scroll-trigger" href="/"><b>Trip Planner</b></a>
+
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
                     <i className="fas fa-bars"></i>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/"><b>Home</b></a></li>
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/destinations"><b>Destinations</b></a></li>
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/about"><b>About</b></a></li>
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/blog"><b>Blog/Gallery</b></a></li>
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/contact"><b>Contact</b></a></li>
-                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/login"><b>Login</b></a></li>
-                        {/* <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/logout"><b>Logout</b></a></li> */}
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/"><b>Home</b></a></li>
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/destinations"><b>Destinations</b></a></li>
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/about"><b>About</b></a></li>
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/blog"><b>Blog</b></a></li>
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/contact"><b>Contact</b></a></li>
+                            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/login"><b>Login</b></a></li>
+                            {/* <li className="nav-item"><a className="nav-link js-scroll-trigger" href="/logout"><b>Logout</b></a></li> */}
                         </ul>
+                    </div>
                 </div>
-            </div>
             </nav>
-            )
-        }
+        )
     }
+}
 export default Header;
