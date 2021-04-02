2f4c49fa07bdcc59a28685ff09101f01ce298861
Sanjeet <sanjeetpandey010@gmail.com>
2021-04-02 22:40:23 +0545

    two image and desc added

--- src/Body/Blog.js
+++ src/Body/Blog.js
@@ -12,15 +12,22 @@ class Blog extends Component{
 					<h3>My First Blog Post</h3>
 					<div class="blog-date">23 Jan 2021</div>
 				</summary>
-				<p></p>
+				<p>Hey its me shyam, Last week we were taken to Lumbini, Nepal where Lord Buddha was born approximately 2600 years ago. 
+                    Before visiting Lumbini, Nepal I was unsure what the place would be like—where we would eat and 
+                    rest as we were so many from our St Joseph School. It was beautiful to see the area where Lord Buddha 
+                    n and all the other temples, we visited around were also very beautiful. n a highly competitive world 
+                    to sell their products and places, we are obviously way behind. We are now trying our best to show the 
+                    spaces left for nature in our gardens and our love to the water, air, biodiversity in all means and the 
+                    love for human-kind as a whole. At this juncture of life when we are thinking value and behaviour of 
+                    humankind more than ever, we would like to update what's happening in Lumbini Buddha Garden.</p>
 			</div>
 			<div class="blog-img">
-				<div class="blog-img-caption"><b>Pictured</b> Some Cool Skyline</div>
+				<div class="blog-img-caption"><b>Pictured by </b>Ram Karki</div>
 			</div>
 			<div class="blog-content">
-				<p></p>
+				<p>When it comes about a beautifully blessed country by Nature, Nepal probably hits the list somewhere at the top. The geographically diverse country is a perfect example of what nature means. From the lush forests of the Terai, the beautiful hills of the Hill and the sky touching glorious mountains in the Himalayans, Nepal is a country that gives the feeling of being closer to nature.</p>
 
-				<p> </p>
+				<p>Every year Nepal is a hot destination for the tourists all around the world for the wonderful beauty it possesses. Among the various beautiful places in Nepal, Illam is the one which should not be missed.</p>
 			</div>	
 	</article>
 </div>
