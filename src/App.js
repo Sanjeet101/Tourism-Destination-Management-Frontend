d3a183aa8a9ffc70d8181af10737884f38c500ec
Sanjeet <sanjeetpandey010@gmail.com>
2021-04-19 22:58:43 +0545

    frontend done

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import Header from './Header/Header'
 import Body from './Body/Body'
 import Footer from './Footer/Footer'
 
-
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
